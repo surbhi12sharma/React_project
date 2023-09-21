a3a1466b992b5b47c96604b13254df072fde1438
surbhi12sharma <surbhisharma938@gmail.com>
2023-09-21 10:02:48 +0530

    Added Export Import

---
+++ src/ComponentType/componnettype.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export function Functiontype(){
+    return(
+        <h1>Function Component Type!</h1>
+    )
+}
+
+
+export default class Classtype extends React.Component{
+    render(){
+        return(
+            <h1>Class Component</h1>
+        )
+    }
+}
