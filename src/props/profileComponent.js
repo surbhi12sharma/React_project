14a62582fb0757699137e8ab1398d93ce587c6ea
surbhi12sharma <surbhisharma938@gmail.com>
2023-09-21 18:17:14 +0530

    Passing object using props

--- src/props/profileComponent.js
+++ src/props/profileComponent.js
@@ -1,8 +1,11 @@
 import ChildCom from "../Day2/child";
+import UserObjCom from "../Day2/userObjCom";
 
 function Profile(props){
+    let user = {userName: 'soumya', userId:102}
     return( <div>
         <ChildCom userName= {props.userName}/>
+        <UserObjCom user = {user} />
         <h3>name = {props.userName} && id = {props.userId}</h3>
     </div>)
 } 
